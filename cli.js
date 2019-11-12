1657b6c6c063b6cbb68738cfd8f5f8b8c5e4c307
Justin Makeig <jmpublic+github@makeig.com>
2019-11-12 09:32:13 -0800

    Adds directory output and better reporting

--- cli.js
+++ cli.js
@@ -1,29 +1,37 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
 const { docopt } = require('docopt');
+const { generateData, envelope } = require('./src/generator.js');
+const name = require('./package.json').name;
 
-const doc = `Generate customer-product-order data.
+const doc = `
+Generate customer-product-order data.
 
 Usage:
-  ${
-		require('./package.json').name
-	} [--orders=<num> --products=<num> --customers=<num>]
+  ${name} [--output DIR]
+  ${name} entities ([--orders=<num> --products=<num> --customers=<num>]) [--output DIR]
 
 Options:
-  -h --help       Show this screen
-  --orders=<num>   Number of orders [default: 100]
-  --products=<num>   Number of products [default: 5]
-  --customers=<num>  Number of customers [default: 25]
+  -h --help           Show this screen
+  --orders=<num>      Number of orders    [default: 100]
+  --products=<num>    Number of products  [default: 5]
+	--customers=<num>   Number of customers [default: 25]
+	-o --output DIR     Output directory
 
 `;
 
 const options = docopt(doc);
+const outputDir = path.resolve(coalesce(options['--output'], 'data'));
 
-// console.log(options);
+function coalesce(...values) {
+	for (const value of values) {
+		if (null !== value && undefined !== value) return value;
+	}
+}
 
-const { generateData, envelope } = require('./src/generator.js');
-
-const fs = require('fs');
+// console.log(outputDir);
 
 const totals = { index: 0, customer: 0, product: 0, order: 0 };
 
@@ -33,8 +41,11 @@ const counts = {
 	products: parseInt(options['--products'], 10),
 	orders: parseInt(options['--orders'], 10)
 };
-process.stdout.write(
-	`Generating ${counts.customers} customers, ${counts.products} products, and ${counts.orders} orders.\n\n`
+
+const diagnostics = process.stderr;
+
+diagnostics.write(
+	`Generating ${counts.customers} customers, ${counts.products} products, and ${counts.orders} orders in ${outputDir}.\n`
 );
 
 /** Total number of entities */
@@ -46,16 +57,22 @@ const total = Object.getOwnPropertyNames(counts).reduce(
 /** Limit to 25 output messages */
 const reportFrequency = Math.floor(total / 25);
 
+const start = process.hrtime();
+
 for (const item of generateData(counts)) {
 	totals[item.type] = totals[item.type] + 1;
 	totals.index = totals.index + 1;
-	if (0 === totals.index % reportFrequency) {
-		process.stdout.write(
+	if (0 === totals.index % reportFrequency || totals.index >= total) {
+		diagnostics.write(
 			`Customers: ${totals.customer}, Products: ${totals.product}, Orders: ${totals.order}\n`
 		);
 	}
 	fs.writeFileSync(
-		`${process.cwd()}/data/${item.type}s/${item.id}.json`,
+		`${outputDir}/${item.type}s/${item.id}.json`,
 		JSON.stringify(envelope(item), null, 2)
 	);
 }
+const duration = process.hrtime(start);
+diagnostics.write(`Generated ${totals.index} files in ${duration[0]}s ${duration[1] / 1000000}ms\n`);
+
+process.exit(0);
