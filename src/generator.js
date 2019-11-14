67c27c786a2bcbb6af061bf0cc4e29b5b17c2c37
Justin Makeig <jmpublic+github@makeig.com>
2019-11-14 12:08:25 -0800

    Updates with proper prettier formatting

--- src/generator.js
+++ src/generator.js
@@ -1,81 +1,82 @@
-const f = require('faker');
+const f = require("faker");
 
-const { pick, several, normal, lognormal } = require('./distributions.js');
+const { pick, several, normal, lognormal } = require("./distributions.js");
 
 function* generateData({ customers, products, orders }) {
-	const customerIDs = [];
-	const productIDs = [];
+  const customerIDs = [];
+  const productIDs = [];
 
-	for (const c of generateCustomers(customers)) {
-		customerIDs.push(c.id);
-		yield c;
-	}
-	for (const p of generateProducts(products)) {
-		productIDs.push(p.id);
-		yield p;
-	}
-	yield* generateOrders(customerIDs, productIDs, orders);
+  for (const c of generateCustomers(customers)) {
+    customerIDs.push(c.id);
+    yield c;
+  }
+  for (const p of generateProducts(products)) {
+    productIDs.push(p.id);
+    yield p;
+  }
+  yield* generateOrders(customerIDs, productIDs, orders);
 }
 
 function* generateCustomers(count = 100) {
-	for (let i = 0; i < count; i++) {
-		const id = f.random.uuid();
-		yield {
-			type: 'customer',
-			id,
-			name: { first: f.name.firstName(), last: f.name.lastName() },
-			address: {
-				street: f.address.streetAddress(),
-				city: f.address.city(),
-				zip: f.address.zipCode()
-			},
-			notes: several(
-				() => ({
-					timestamp: f.date.recent(),
-					text: f.lorem.sentence()
-				}),
-				() => lognormal(0, 1.5)
-			),
-			verified: f.random.boolean()
-		};
-	}
+  for (let i = 0; i < count; i++) {
+    const id = f.random.uuid();
+    yield {
+      type: "customer",
+      id,
+      name: { first: f.name.firstName(), last: f.name.lastName() },
+      address: {
+        street: f.address.streetAddress(),
+        city: f.address.city(),
+        zip: f.address.zipCode()
+      },
+      notes: several(
+        () => ({
+          timestamp: f.date.recent(),
+          text: f.lorem.sentence()
+        }),
+        () => lognormal(0, 1.5)
+      ),
+      verified: f.random.boolean()
+    };
+  }
 }
 
 function* generateProducts(count = 100) {
-	for (let i = 0; i < count; i++) {
-		const id = f.random.uuid();
-		yield {
-			type: 'product',
-			id
-		};
-	}
+  for (let i = 0; i < count; i++) {
+    const id = f.random.uuid();
+    yield {
+      type: "product",
+      id,
+      unitPrice: Math.ceil(normal(50, 15), 0.01)
+    };
+  }
 }
 
 function* generateOrders(customers, products, count = 1000) {
-	for (let i = 0; i < count; i++) {
-		const id = f.random.uuid();
-		yield {
-			type: 'order',
-			id,
-			customer: pick(customers),
-			lines: several(
-				() => ({
-					product: pick(products),
-					quantity: Math.max(1, Math.floor(normal(100, 60)))
-				}),
-				() => lognormal(0, 1.5)
-			)
-		};
-	}
+  for (let i = 0; i < count; i++) {
+    const id = f.random.uuid();
+    yield {
+      type: "order",
+      id,
+      customer: pick(customers),
+      lines: several(
+        () => ({
+          product: pick(products),
+          quantity: Math.max(1, Math.floor(normal(100, 60)))
+        }),
+        () => lognormal(0, 1.5)
+      )
+    };
+  }
 }
 
 function envelope(entity) {
-	return {
-		headers: {
-			type: entity.type
-		},
-		instance: entity
-	};
+  return {
+    headers: {
+      type: entity.type
+    },
+    instance: entity
+  };
 }
 
 module.exports = { generateData, envelope };
