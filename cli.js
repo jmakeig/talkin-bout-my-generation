eea7aeb2489b028b7d6b0ed784685ae1afb9c6a0
Justin Makeig <jmpublic+github@makeig.com>
2019-11-18 15:59:22 -0800

    Tabs to spaces

--- cli.js
+++ cli.js
@@ -16,7 +16,7 @@ Usage:
 Options:
   -h --help           Show this screen
   --orders=<num>      Number of orders    [default: 100]
-	--products=<num>    Number of products  [default: 5]
+  --products=<num>    Number of products  [default: 5]
   --customers=<num>   Number of customers [default: 25]
   -o --output DIR     Output directory
 `;
